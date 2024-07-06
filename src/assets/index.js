346ebe052c88eb3b6167ba01b593ce51158c9894
unknown <vibhudendra.s@gmail.com>
2024-07-06 18:10:33 +0530

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,16 @@
+import linkIcon from './link.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import logo from './logo.svg'
+import tick from './tick.svg'
+import logo2 from './logo2.svg'
+
+
+export {
+    linkIcon,
+    loader,
+    copy,
+    logo,
+    tick,
+    logo2,
+}
